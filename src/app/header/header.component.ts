0de102387d51da9c6ff04c33916ba5d15332b607
Martha Plantz <maplantz89@gmail.com>
2021-03-15 10:29:59 -0700

    updating to github

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -17,4 +17,4 @@ export class HeaderComponent implements OnInit {
     })
   }
 
-}
+} 
--- src/app/services/observables/common-observables.service.ts
+++ src/app/services/observables/common-observables.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonObservableService {
   //behavior subjects are items you can call next on to set values to it; next and getValue => look up later
-  private _pageTitle = new BehaviorSubject<string>(null); 
+  private _pageTitle = new Subject<string>(); 
   pageTitle: Observable<string> = this._pageTitle.asObservable();
 
   constructor() { }
